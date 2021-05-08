47965178b7c95ff0da37c3349ccae7724da3e1e3
Code4Blessings <robincw35@gmail.com>
2021-05-08 18:08:20 -0400

    Completed UI for Welcome Screen

--- assets/colors.js
+++ assets/colors.js
@@ -2,6 +2,7 @@ const colors = {
     bgSuccess: '#a5deba',
     bgError: '#deada5',
     bgPrimary: '#AAD1E6',
+    bgMain: '#2E424D',
     bgTextInput: '#ececec',
     txtPlaceHolder: 'grey',
     borderColor: '#E9E9E9',
--- screens/AppSwitchNavigator/WelcomeScreen.js
+++ screens/AppSwitchNavigator/WelcomeScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 
 import colors from '../../assets/colors';
+import CustomActionButton from '../../components/CustomActionButton';
 
 export default class WelcomeScreen extends Component {
   constructor(props) {
@@ -14,15 +15,55 @@ export default class WelcomeScreen extends Component {
   render() {
     return (
       <View style={styles.welcomeContainer}>
-        <View style={{flex: 1, borderColor: 'black', borderWidth: 1}}>
-            <Ionicons name='ios-bookmarks' size={150}color={colors.logoColor} />
+        <View style={styles.logoContainer}>
+            <Ionicons name='ios-book' size={150}color={colors.logoColor} />
+            <Text style={styles.appName}>BOOK WORM</Text>
         </View>
-        <View style={{flex: 1, borderColor: 'orange', borderWidth: 1}}></View>
+        <View style={styles.buttonContainer}>
+          <CustomActionButton 
+            style={styles.loginButton}
+            title='Login' 
+            onPress={() => alert('Login')
+          }>
+            <Text style={styles.loginButtonText} >Log In</Text>
+          </CustomActionButton>
+          <CustomActionButton 
+            style={styles.signUpButton}
+            title='Signup' 
+            onPress={() => alert('Sign Up')
+          }>
+            <Text style={styles.loginButtonText}>Sign Up</Text>
+          </CustomActionButton>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    welcomeContainer: {flex: 1}
+    welcomeContainer: {flex: 1, backgroundColor: colors.bgMain},
+    logoContainer: {
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center'
+    },
+    buttonContainer: {flex: 1, alignItems: 'center' },
+
+    appName: {fontSize: 50, fontWeight: '100', color: 'white'},
+
+    loginButton: {
+      width: 200, 
+      backgroundColor: 'transparent', 
+      borderWidth: 0.5, 
+      borderColor: colors.bgPrimary,
+      marginBottom: 10
+    },
+    loginButtonText: {fontWeight: '100', color: 'white' },
+
+    signUpButton : {
+      width: 200, 
+      backgroundColor: 'transparent', 
+      borderWidth: 0.5, 
+      borderColor: colors.bgError
+    }
 });
